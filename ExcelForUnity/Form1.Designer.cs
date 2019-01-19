dd21ab83bb2cc454e5e7941700f368abd2be4c6d
Ragıp Hatunoğlu <rhmaras@gmail.com>
2019-01-19 23:18:25 +0300

    Project 1.0
    Converts excel files to json and c# for unity.

---
+++ ExcelForUnity/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ExcelForUnity
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ButtonLoad = new System.Windows.Forms.Button();
+            this.ButtonConvertToJson = new System.Windows.Forms.Button();
+            this.ButtonConvertToC = new System.Windows.Forms.Button();
+            this.CheckBoxIsSerializable = new System.Windows.Forms.CheckBox();
+            this.LabelFileName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelJSonConverted = new System.Windows.Forms.Label();
+            this.labelCSharpConverted = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonLoad
+            // 
+            this.ButtonLoad.Location = new System.Drawing.Point(12, 12);
+            this.ButtonLoad.Name = "ButtonLoad";
+            this.ButtonLoad.Size = new System.Drawing.Size(134, 38);
+            this.ButtonLoad.TabIndex = 0;
+            this.ButtonLoad.Text = "Load Excel File";
+            this.ButtonLoad.UseVisualStyleBackColor = true;
+            this.ButtonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
+            // ButtonConvertToJson
+            // 
+            this.ButtonConvertToJson.Location = new System.Drawing.Point(12, 56);
+            this.ButtonConvertToJson.Name = "ButtonConvertToJson";
+            this.ButtonConvertToJson.Size = new System.Drawing.Size(134, 38);
+            this.ButtonConvertToJson.TabIndex = 1;
+            this.ButtonConvertToJson.Text = "Convert To JSon";
+            this.ButtonConvertToJson.UseVisualStyleBackColor = true;
+            this.ButtonConvertToJson.Click += new System.EventHandler(this.ButtonConvertToJson_Click);
+            // 
+            // ButtonConvertToC
+            // 
+            this.ButtonConvertToC.Location = new System.Drawing.Point(12, 100);
+            this.ButtonConvertToC.Name = "ButtonConvertToC";
+            this.ButtonConvertToC.Size = new System.Drawing.Size(134, 37);
+            this.ButtonConvertToC.TabIndex = 2;
+            this.ButtonConvertToC.Text = "Convert To C#";
+            this.ButtonConvertToC.UseVisualStyleBackColor = true;
+            this.ButtonConvertToC.Click += new System.EventHandler(this.ButtonConvertToCSharp_Click);
+            // 
+            // CheckBoxIsSerializable
+            // 
+            this.CheckBoxIsSerializable.AutoSize = true;
+            this.CheckBoxIsSerializable.Location = new System.Drawing.Point(29, 143);
+            this.CheckBoxIsSerializable.Name = "CheckBoxIsSerializable";
+            this.CheckBoxIsSerializable.Size = new System.Drawing.Size(99, 17);
+            this.CheckBoxIsSerializable.TabIndex = 3;
+            this.CheckBoxIsSerializable.Text = "Create Serialize";
+            this.CheckBoxIsSerializable.UseVisualStyleBackColor = true;
+            // 
+            // LabelFileName
+            // 
+            this.LabelFileName.AutoSize = true;
+            this.LabelFileName.Location = new System.Drawing.Point(152, 25);
+            this.LabelFileName.Name = "LabelFileName";
+            this.LabelFileName.Size = new System.Drawing.Size(90, 13);
+            this.LabelFileName.TabIndex = 4;
+            this.LabelFileName.Text = "File not selected..";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(12, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 54);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Excel cell format type samples :\r\nstring:  (Cell Type is general) test,Test,100, " +
+    "etc.\r\nfloat:    (Cell Type is general) 200f,3f,0.4f, etc.\r\nint:       (Cell Type" +
+    " is number) 10,300,-200, etc.\r\n";
+            // 
+            // labelJSonConverted
+            // 
+            this.labelJSonConverted.AutoSize = true;
+            this.labelJSonConverted.Location = new System.Drawing.Point(152, 69);
+            this.labelJSonConverted.Name = "labelJSonConverted";
+            this.labelJSonConverted.Size = new System.Drawing.Size(75, 13);
+            this.labelJSonConverted.TabIndex = 6;
+            this.labelJSonConverted.Text = "Not converted";
+            // 
+            // labelCSharpConverted
+            // 
+            this.labelCSharpConverted.AutoSize = true;
+            this.labelCSharpConverted.Location = new System.Drawing.Point(152, 112);
+            this.labelCSharpConverted.Name = "labelCSharpConverted";
+            this.labelCSharpConverted.Size = new System.Drawing.Size(75, 13);
+            this.labelCSharpConverted.TabIndex = 7;
+            this.labelCSharpConverted.Text = "Not converted";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 240);
+            this.Controls.Add(this.labelCSharpConverted);
+            this.Controls.Add(this.labelJSonConverted);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LabelFileName);
+            this.Controls.Add(this.CheckBoxIsSerializable);
+            this.Controls.Add(this.ButtonConvertToC);
+            this.Controls.Add(this.ButtonConvertToJson);
+            this.Controls.Add(this.ButtonLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Excel Converter For Unity";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonLoad;
+        private System.Windows.Forms.Button ButtonConvertToJson;
+        private System.Windows.Forms.Button ButtonConvertToC;
+        private System.Windows.Forms.CheckBox CheckBoxIsSerializable;
+        private System.Windows.Forms.Label LabelFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelJSonConverted;
+        private System.Windows.Forms.Label labelCSharpConverted;
+    }
+}
+
---
+++ ExcelForUnity/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using ExcelDataReader;
+using System.Data.Entity.Design.PluralizationServices;
+using System.Globalization;
+
+namespace ExcelForUnity
+{
+    public partial class Form1 : Form
+    {
+        FileStream fileStream;
+        bool isFileLoaded;
+
+        DataSet result;
+        
+        Dictionary<int, Type[,]> cellTypes;
+
+        string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|*.xls;*.xlsx", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        fileStream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read); //Dosya Kullanımdaysa hata veriyor..
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        return;
+                    }
+
+                    isFileLoaded = true;
+                    LabelFileName.Text = ofd.SafeFileName;
+                    path = ofd.FileName.Replace(ofd.SafeFileName, "");
+                    IExcelDataReader reader = ExcelReaderFactory.CreateReader(fileStream);
+                    result = reader.AsDataSet();
+
+                    cellTypes = new Dictionary<int, Type[,]>();
+
+                    for (int workSheetIndex = 0; workSheetIndex < reader.ResultsCount; workSheetIndex++)
+                    {
+                        Type[,] types = new Type[reader.RowCount, reader.FieldCount];
+                        int rowIndex = 0;
+                        while (reader.Read())
+                        {
+                            for (int columnIndex = 0; columnIndex <= reader.FieldCount - 1; columnIndex++)
+                            {
+                                types[rowIndex, columnIndex] = reader.GetFieldType(columnIndex);
+                            }
+                            rowIndex++;
+                        }
+                        cellTypes.Add(workSheetIndex, types);
+                        reader.NextResult();
+                    }
+
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+            }
+        }
+
+        private void ButtonConvertToJson_Click(object sender, EventArgs e)
+        {
+            if (isFileLoaded == false)
+                return;
+
+            string TEXTJSON = "{\r\n";
+
+            string comma;
+            List<string> headers;
+            string items = "";
+
+            for (int tableIndex = 0; tableIndex < result.Tables.Count; tableIndex++)
+            {
+                TEXTJSON += "\t\"" + result.Tables[tableIndex].TableName + "\": [\r\n";
+
+                var table = result.Tables[tableIndex];
+
+                headers = new List<string>();
+
+                for (int columnIndex = 0; columnIndex < table.Columns.Count; columnIndex++)
+                {
+                    headers.Add("\"" + table.Rows[0][columnIndex] + "\": ");
+                }
+
+                items = "";
+
+                for (int rowIndex = 1; rowIndex < table.Rows.Count; rowIndex++)
+                {
+                    items += "\t\t{\r\n";
+                    for (int columnIndex = 0; columnIndex < table.Columns.Count; columnIndex++)
+                    {
+                        comma = "";
+                        if (columnIndex < table.Columns.Count - 1)
+                        {
+                            comma += ",";
+                        }
+                        string text = "\t\t\t" + headers[columnIndex] + GetFixedType(tableIndex, table, rowIndex, columnIndex) + comma + "\r\n";
+                        items += text;
+                    }
+                    comma = "";
+                    if (rowIndex < table.Rows.Count - 1)
+                    {
+                        comma += ",";
+                    }
+                    items += "\t\t}" + comma + "\r\n";
+                }
+
+                comma = "";
+                if (tableIndex < result.Tables.Count - 1)
+                {
+                    comma += ",";
+                }
+                TEXTJSON += items + "\t]" + comma + "\r\n";
+            }
+
+            TEXTJSON += "}";
+
+            string fileName = path + "/JSonData.txt";
+
+            try
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                fs.Close();
+                File.AppendAllText(fileName, Environment.NewLine + TEXTJSON);
+                labelJSonConverted.Text = "Converted!";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string GetFixedType(int tableIndex, DataTable table, int rowIndex, int columnIndex)
+        {
+            string text = "";
+            Type type = cellTypes[tableIndex][rowIndex, columnIndex];
+
+            string target = "" + table.Rows[rowIndex][columnIndex];
+
+            if (type.Equals(typeof(System.String)))
+            {
+                if (target.Substring(target.Length - 1).Equals("f"))
+                {
+                    float v = 0;
+                    if (float.TryParse(target.Substring(0, target.Length - 1), out v))
+                    {
+                        text = target.Substring(0, target.Length - 1);
+                    }
+                    else
+                    {
+                        text = "\"" + table.Rows[rowIndex][columnIndex] + "\"";
+                    }
+                }
+                else
+                {
+                    text = "\"" + table.Rows[rowIndex][columnIndex] + "\"";
+                }
+            }
+            if (type.Equals(typeof(System.Double)))
+            {
+                text = "" + table.Rows[rowIndex][columnIndex];
+            }
+            if (type.Equals(typeof(System.DateTime)))
+            {
+                text = "" + table.Rows[rowIndex][columnIndex];
+            }
+
+            return text;
+        }
+
+        private void ButtonConvertToCSharp_Click(object sender, EventArgs e)
+        {
+            if (isFileLoaded == false)
+                return;
+
+            string serializeText = "";
+            if (CheckBoxIsSerializable.Checked)
+            {
+                serializeText = "[Serializable]\r\n";
+            }
+
+            PluralizationService pluralizationService = PluralizationService.CreateService(new CultureInfo("en-us"));
+
+            string TEXTCSHARP = "";
+
+            string items = "";
+
+            for (int tableIndex = 0; tableIndex < result.Tables.Count; tableIndex++)
+            {
+                items = serializeText;
+                items += "public class " + pluralizationService.Singularize("" + result.Tables[tableIndex].TableName) + "\r\n{";
+
+                var table = result.Tables[tableIndex];
+
+                for (int columnIndex = 0; columnIndex < table.Columns.Count; columnIndex++)
+                {
+                    items += "\r\n\tpublic " + GetVariableType(tableIndex, table, columnIndex) + " " + table.Rows[0][columnIndex] + ";";
+                }
+
+                items += "\r\n}";
+                TEXTCSHARP += items + "\r\n\r\n";
+            }
+
+            items = serializeText + "public class RootObject\r\n{\r\n";
+            for (int tableIndex = 0; tableIndex < result.Tables.Count; tableIndex++)
+            {
+                items += "\tpublic List<" + pluralizationService.Singularize("" + result.Tables[tableIndex].TableName) + "> " + result.Tables[tableIndex].TableName + ";\r\n";
+            }
+
+            items += "}";
+            TEXTCSHARP += items;
+
+            string fileName = path + "/CSharpData.txt";
+
+            try
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                fs.Close();
+                File.AppendAllText(fileName, Environment.NewLine + TEXTCSHARP);
+                labelCSharpConverted.Text = "Converted!";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private string GetVariableType(int tableIndex, DataTable table, int columnIndex)
+        {
+            Type type = cellTypes[tableIndex][1, columnIndex];
+
+            if (table.Rows[1][columnIndex] == null)
+                return "object";
+
+            string target = "" + table.Rows[1][columnIndex];
+
+            if (type.Equals(typeof(System.String)))
+            {
+                if (target.Substring(target.Length - 1).Equals("f"))
+                {
+                    float v = 0;
+                    if (float.TryParse(target.Substring(0, target.Length - 1), out v))
+                    {
+                        return "float";
+                    }
+                    else
+                    {
+                        return "string";
+                    }
+                }
+                else
+                {
+                    return "string";
+                }
+            }
+            if (type.Equals(typeof(System.Double)))
+            {
+                return "int";
+            }
+            if (type.Equals(typeof(System.DateTime)))
+            {
+                return "DateTime";
+            }
+
+            return "Object";
+        }
+    }
+}
